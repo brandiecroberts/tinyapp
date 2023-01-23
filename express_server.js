105ca5286011e3ed6aca8721722b7e96b737bb38
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-23 15:04:49 -0800

    Updated express server and added an index

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const urls = {urls: urlDatabase};
+  res.render("urls_index", urls);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -23,3 +28,29 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// const express = require("express");
+// const app = express();
+// const PORT = 8080; // default port 8080
+
+// const urlDatabase = {
+//   b2xVn2: "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com",
+// };
+// app.get("/urls", (req, res) => {
+//   const urls = { urls: urlDatabase };
+//   res.render("urls_index/", urls);
+// });
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}!`);
+// });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
