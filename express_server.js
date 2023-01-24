c291c3a6af55ea675215873d5969091ea1d2b107
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-23 16:59:01 -0800

    Updated code to redirect user using short url

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,43 +15,54 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", urls);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// const express = require("express");
-// const app = express();
-// const PORT = 8080; // default port 8080
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const value = req.body.longURL;
+  const key = generateRandomString();
+  urlDatabase[key] = value;
+  res.redirect(`/urls/${key}`);
+});
 
-// const urlDatabase = {
-//   b2xVn2: "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com",
-// };
-// app.get("/urls", (req, res) => {
-//   const urls = { urls: urlDatabase };
-//   res.render("urls_index/", urls);
-// });
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+const generateRandomString = function() {
+  let result = "";
+  const len = 6;
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = len; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+console.log(generateRandomString());
