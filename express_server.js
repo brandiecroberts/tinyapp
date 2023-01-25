22a85843f27426e68eb75ef230c4e851dad7c2a6
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-24 17:30:27 -0800

    Adding delete/edit buttons to webpage

--- express_server.js
+++ express_server.js
@@ -2,59 +2,69 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
-app.get("/urls", (req, res) => {
-  const urls = {urls: urlDatabase};
-  res.render("urls_index", urls);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id]
-  };
-  res.render("urls_show", templateVars);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
+  console.log(req.body); // Log the POST request body to the console
   const value = req.body.longURL;
   const key = generateRandomString();
   urlDatabase[key] = value;
   res.redirect(`/urls/${key}`);
 });
-
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const deleteKey = req.params.id;
+  delete urlDatabase[deleteKey];
+  res.redirect("/urls");
+});
+app.post("/urls/:id", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const key = req.params.id;
+  urlDatabase[key] = req.body.longUrl;
+  res.redirect("/urls");
+});
+
+
 
 const generateRandomString = function() {
   let result = "";
@@ -65,4 +75,3 @@ const generateRandomString = function() {
     result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 };
-console.log(generateRandomString());
